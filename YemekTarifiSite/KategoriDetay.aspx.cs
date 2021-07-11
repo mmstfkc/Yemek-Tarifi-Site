b5ea67fa90c4919d78b3c57bae7cabb22334d184
mmstfkc <36615411+mmstfkc@users.noreply.github.com>
2021-07-11 15:05:16 +0300

    Detay kodlaması tamamlandı

--- YemekTarifiSite/KategoriDetay.aspx.cs
+++ YemekTarifiSite/KategoriDetay.aspx.cs
@@ -4,14 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data.SqlClient;
 namespace YemekTarifiSite
 {
     public partial class KategoriDetay : System.Web.UI.Page
     {
+        sqlsinif bgl = new sqlsinif();
+        string kategoriid = "";
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            kategoriid = Request.QueryString["Kategoriid"];
+            SqlCommand komut = new SqlCommand("select * from tbl_yemekler where kategoriid=@p1", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", kategoriid);
+            SqlDataReader rd = komut.ExecuteReader();
+            DataList2.DataSource = rd;
+            DataList2.DataBind();
         }
     }
 }
